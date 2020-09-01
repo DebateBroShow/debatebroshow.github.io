6a9a34b04ef43b88b8c6a8bf49c685d00db1ff56
Eoin <debatebroshow@gmail.com>
2020-08-31 23:19:00 -0400

    mobile navbar

--- scripts/loadcontent.js
+++ scripts/loadcontent.js
@@ -1,4 +1,5 @@
 $(function () {
     $("#navbar").load("https://debatebroshow.github.io/components/navbar.html");
+    $("#navmodal").load("https://debatebroshow.github.io/components/mobileNavbar.html");
     $("#footer").load("https://debatebroshow.github.io/components/footer.html");
 });
