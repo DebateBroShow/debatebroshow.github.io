e11d50a8ae39cf294d4ec7bc0e4d1f3c7bfd7450
Eoin <debatebroshow@gmail.com>
2020-08-31 23:53:42 -0400

    better experience?

--- scripts/loadcontent.js
+++ scripts/loadcontent.js
@@ -7,13 +7,14 @@ $(function () {
         isMobile = true;
     }
     console.log(isMobile);
+    $("#navmodal").load("https://debatebroshow.github.io/components/mobileNavbar.html");
+    $("#navbar").load("https://debatebroshow.github.io/components/navbar.html");
+    
     if(isMobile){
-        $("#navbar").remove()
-        $("#navmodal").load("https://debatebroshow.github.io/components/mobileNavbar.html");
+        $("#navbar").show()
     }
     else{
-        $("#mobilenavbar").remove();
-        $("#navbar").load("https://debatebroshow.github.io/components/navbar.html");
+        $("#mobilenavbar").show();   
     }
 
     $("#footer").load("https://debatebroshow.github.io/components/footer.html");
