2f1e6dffe8c3a9958742c3c4540bbefd07f18889
Eoin <debatebroshow@gmail.com>
2020-10-20 11:20:33 -0400

    borken line

--- AutomationTools/automation.py
+++ AutomationTools/automation.py
@@ -53,7 +53,7 @@ def GenerateFiles(epsiodefilename, cardfilename, pagefilename, maincardfilename,
     shutil.move("sitemap.xml", "..")
 
 def cleanAutomation(filename1, filename2):
-    print(not os.listdir("./GeneratedPages/"+filename1))
+    #print(not os.listdir("./GeneratedPages/"+filename1))
     if (os.path.exists("./GeneratedPages/"+filename1) and os.listdir("./GeneratedPages/"+filename1)) or os.path.exists("../pages/episodeList/"+filename2):
         if os.path.exists("./GeneratedPages/"+filename1): 
             shutil.rmtree("./GeneratedPages/"+filename1)
@@ -61,8 +61,8 @@ def cleanAutomation(filename1, filename2):
             os.remove("../pages/episodeList/"+filename2)
         #todo: restore the other 2 files from backup perhaps?
     else:
-        if not os.listdir("./GeneratedPages/"+filename1):
-            shutil.rmtree("./GeneratedPages/"+filename1)
+        #if not os.listdir("./GeneratedPages/"+filename1):
+         #   shutil.rmtree("./GeneratedPages/"+filename1)
         shutil.copyfile("../pages/episodeIndex.html", "episodeIndex.backup")
         shutil.copyfile("../index.html", "index.backup")
 
