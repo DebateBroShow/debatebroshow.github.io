eca2393334718d28b9245ea4f814a6f09154ced2
Eoin <debatebroshow@gmail.com>
2020-10-22 01:38:31 -0400

    testeets

--- AutomationTools/automation.py
+++ AutomationTools/automation.py
@@ -104,27 +104,20 @@ def updateEpisodeIndex(filename):
         print(len(card))
         for line in file.readlines():
             i += 1
-            print("line: " + str(i) + "\n" + str(line))
+            #print("line: " + str(i) + "\n" + str(line))
             if "id=\"marker\"" in line:
-                print(i)
                 if k == 0:
                     k = i
-                print(j)
                 newIndex.append(card[j])
                 oldEpisode.append((line))
                 j += 1
             else:
                 newIndex.append(line)
-        print(newIndex[k-1])
-        print(newIndex[k])
-        print(newIndex[k+1])
-        print(newIndex[k+47])
-        print(newIndex[k+48])
-    #newIndex = newIndex[:k+18] + oldEpisode + newIndex[k+18:]
-    # open(indexFilename, 'w').close()
-    # with open(indexFilename, "w") as file:
-    #     for line in newIndex:
-    #         file.write(line)
+    newIndex = newIndex[:k] + oldEpisode + newIndex[k]
+    open(indexFilename, 'w').close()
+    with open(indexFilename, "w") as file:
+         for line in newIndex:
+             file.write(line)
 
 #reigon Gets the data from the
 def generateCard(info, filename):
