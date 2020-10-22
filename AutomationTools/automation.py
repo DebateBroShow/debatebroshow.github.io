3ac7f63e2970361cc0623d28ed6bd04fa896c527
Eoin <debatebroshow@gmail.com>
2020-10-22 00:38:28 -0400

    autopep8 baby

--- AutomationTools/automation.py
+++ AutomationTools/automation.py
@@ -4,7 +4,6 @@ import shutil
 import time
 from sitemap import *
 
-#can't believe this works
 def GenerateFiles(epsiodefilename, cardfilename, pagefilename, maincardfilename, folderName):
     episodeInfo = parseEpisode(episodefilename)
 
@@ -12,62 +11,65 @@ def GenerateFiles(epsiodefilename, cardfilename, pagefilename, maincardfilename,
     newMainCardFilename = episodeInfo[0]+"_MainCard.html"
     newPageFilename = episodeInfo[0]+".html"
 
-    src_dir = os.getcwd() 
-    #clean up, in case files had been generated previously, to avoid errors
-    #creates a backup of the edited files the first time only
+    src_dir = os.getcwd()
+    # clean up, in case files had been generated previously, to avoid errors
+    # creates a backup of the edited files the first time only
     print("cleaning up previous automation attempts")
     cleanAutomation(episodeInfo[0], newPageFilename)
-    #time.sleep(15) #to see if automated deleting is working 
+    # time.sleep(15) #to see if automated deleting is working
 
-    #start generating pages
+    # start generating pages
     print("creating copies of the files to edit")
     os.makedirs("./GeneratedPages/"+episodeInfo[0])
     shutil.copyfile(cardfilename, newCardFilename)
     shutil.copyfile(pagefilename, newPageFilename)
     shutil.copyfile(maincardfilename, newMainCardFilename)
 
-    #generates html code
+    # generates html code
     print("generating new html files")
     generateCard(episodeInfo, newCardFilename)
     generateMainCard(episodeInfo, newMainCardFilename)
     generatePage(episodeInfo, newPageFilename)
 
     print("automatically updating the html of the website")
-    #updating and moving files automatically
-    #copies the generated episode file to the main site
+    # updating and moving files automatically
+    # copies the generated episode file to the main site
     shutil.copyfile(newPageFilename, "../pages/episodeList/"+newPageFilename)
 
-    #copies the card into episodeIndex.html
+    # copies the card into episodeIndex.html
     updateIndex(newMainCardFilename)
     updateEpisodeIndex(newCardFilename)
 
     print("cleaing up remaining files")
-    #file organization
+    # file organization
     shutil.move(newCardFilename, "./GeneratedPages/"+episodeInfo[0])
     shutil.move(newPageFilename, "./GeneratedPages/"+episodeInfo[0])
     shutil.move(newMainCardFilename, "./GeneratedPages/"+episodeInfo[0])
 
     mainRoutine()
-    if os.path.exists("../sitemap.xml"): 
-            os.remove("../sitemap.xml")
+    if os.path.exists("../sitemap.xml"):
+        os.remove("../sitemap.xml")
     shutil.move("sitemap.xml", "..")
+
 
 def cleanAutomation(filename1, filename2):
     #print(not os.listdir("./GeneratedPages/"+filename1))
     if (os.path.exists("./GeneratedPages/"+filename1) and os.listdir("./GeneratedPages/"+filename1)) or os.path.exists("../pages/episodeList/"+filename2):
-        if os.path.exists("./GeneratedPages/"+filename1): 
+        if os.path.exists("./GeneratedPages/"+filename1):
             shutil.rmtree("./GeneratedPages/"+filename1)
-        if os.path.exists("../pages/episodeList/"+filename2): 
+        if os.path.exists("../pages/episodeList/"+filename2):
             os.remove("../pages/episodeList/"+filename2)
-        #todo: restore the other 2 files from backup perhaps?
+        # todo: restore the other 2 files from backup perhaps?
     else:
         if os.path.exists("./GeneratedPages/"+filename1):
-           shutil.rmtree("./GeneratedPages/"+filename1)
+            shutil.rmtree("./GeneratedPages/"+filename1)
         shutil.copyfile("../pages/episodeIndex.html", "episodeIndex.backup")
         shutil.copyfile("../index.html", "index.backup")
 
-#updates the homepage automatically, works really great
-def updateIndex(filename):  
+# updates the homepage automatically, works really great
+
+
+def updateIndex(filename):
     indexFilename = "../index.html"
     i = -1
     j = 0
@@ -86,8 +88,10 @@ def updateIndex(filename):
         for line in newIndex:
             file.write(line)
 
-#updates the index, some indentation gets messed up, but appears to work overall
-def updateEpisodeIndex(filename):  
+# updates the index, some indentation gets messed up, but appears to work overall
+
+
+def updateEpisodeIndex(filename):
     indexFilename = "../pages/episodeIndex.html"
     i = -1
     j = 0
@@ -112,9 +116,11 @@ def updateEpisodeIndex(filename):
     open(indexFilename, 'w').close()
     with open(indexFilename, "w") as file:
         for line in newIndex:
-            file.write(line) 
+            file.write(line)
 
-#these 3 generate html code
+# these 3 generate html code
+
+
 def generateCard(info, filename):
     EPISODEFILENAME = info[0]
     IMAGENAME = info[1]
@@ -123,14 +129,15 @@ def generateCard(info, filename):
 
     with open(filename, 'r') as file:
         filedata = file.read()
-    
+
     filedata = filedata.replace('EPISODEFILENAME', EPISODEFILENAME)
     filedata = filedata.replace('IMAGENAME', IMAGENAME)
     filedata = filedata.replace('EPISODETITLE', EPISODETITLE)
-    filedata = filedata.replace('EPISODESUBTITLE',EPISODESUBTITLE)
+    filedata = filedata.replace('EPISODESUBTITLE', EPISODESUBTITLE)
 
     with open(filename, 'w') as file:
         file.write(filedata)
+
 
 def generateMainCard(info, filename):
     EPISODEFILENAME = info[0]
@@ -140,14 +147,15 @@ def generateMainCard(info, filename):
 
     with open(filename, 'r') as file:
         filedata = file.read()
-    
+
     filedata = filedata.replace('EPISODEFILENAME', EPISODEFILENAME)
     filedata = filedata.replace('IMAGENAME', IMAGENAME)
     filedata = filedata.replace('EPISODETITLE', EPISODETITLE)
-    filedata = filedata.replace('EPISODESUBTITLE',EPISODESUBTITLE)
+    filedata = filedata.replace('EPISODESUBTITLE', EPISODESUBTITLE)
 
     with open(filename, 'w') as file:
         file.write(filedata)
+
 
 def generatePage(info, filename):
     EPISODEFILENAME = info[0]
@@ -161,44 +169,54 @@ def generatePage(info, filename):
 
     with open(filename, 'r') as file:
         filedata = file.read()
-    
+
+    filedata = editPageData(filedata, EPISODEFILENAME, IMAGENAME, EPISODETITLE,
+                            EPISODESUBTITLE, PAGETITLE, BUZZSPROUTURL, SPOTIFYURL, YOUTUBEURL)
+
+    with open(filename, 'w') as file:
+        file.write(filedata)
+
+
+def editPageData(filedata, EPISODEFILENAME, IMAGENAME, EPISODETITLE, EPISODESUBTITLE, PAGETITLE, BUZZSPROUTURL, SPOTIFYURL, YOUTUBEURL):
     filedata = filedata.replace('EPISODEFILENAME', EPISODEFILENAME)
     filedata = filedata.replace('IMAGENAME', IMAGENAME)
     filedata = filedata.replace('EPISODETITLE', EPISODETITLE)
-    filedata = filedata.replace('EPISODESUBTITLE',EPISODESUBTITLE)
+    filedata = filedata.replace('EPISODESUBTITLE', EPISODESUBTITLE)
     filedata = filedata.replace('PAGETITLE', PAGETITLE)
     filedata = filedata.replace('BUZZSPROUTURL', BUZZSPROUTURL)
     filedata = filedata.replace('SPOTIFYURL', SPOTIFYURL)
     filedata = filedata.replace('YOUTUBEURL', YOUTUBEURL)
+    return filedata
 
-    with open(filename, 'w') as file:
-        file.write(filedata)
-    
-#hope this works lol, not many edge cases handled, i thinks its only currently windows
+# hope this works lol, not many edge cases handled, i thinks its only currently windows
+
+
 def parseEpisode(episodefilename):
     episode = open(episodefilename, "r+")
     lines = [line for line in episode.readlines()]
     linesParsed = []
     for _ in lines:
-        if _[-2:] == "\r\n": 
+        if _[-2:] == "\r\n":
             linesParsed.append(_[0:-2])
             continue
-        elif _[-1:] == "\n": 
+        elif _[-1:] == "\n":
             linesParsed.append(_[0:-1])
             continue
         else:
             linesParsed.append(_)
     return linesParsed[linesParsed.index("---")+1:]
 
+
 if __name__ == "__main__":
     print("Type y to confirm you actually read and understood all the steps in the how to use this text file, anything else with exit safely :)")
-    text = input() #is this python 2 only?
+    text = input()  # is this python 2 only?
     if(text != "y" and text != "Y"):
         exit()
     print("TODO: fix the cleanup with episodeIndex, it appends episodes more than once")
-    episodefilename="episode.txt"
-    cardfilename="card.html"
-    maincardfilename="mainCard.html"
-    pagefilename="page.html"
-    folderName="GeneratedPages"
-    GenerateFiles(episodefilename,cardfilename,pagefilename,maincardfilename,folderName)
+    episodefilename = "episode.txt"
+    cardfilename = "card.html"
+    maincardfilename = "mainCard.html"
+    pagefilename = "page.html"
+    folderName = "GeneratedPages"
+    GenerateFiles(episodefilename, cardfilename,
+                  pagefilename, maincardfilename, folderName)
